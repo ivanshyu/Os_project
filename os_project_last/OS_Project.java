ab98a5e433eda6ccf0cd3fba9482099c2c820bb1
dream85123 <31991882+dream85123@users.noreply.github.com>
2017-12-26 00:53:10 +0800

    Add files via upload

---
+++ os_project_last/OS_Project.java
@@ -0,0 +1,30 @@
+package os_project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+
+public class OS_Project extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("bridge.fxml"));
+        stage.setTitle("OS_Project");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
